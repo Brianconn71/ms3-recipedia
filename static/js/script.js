f11d8abd67699553bd11aae3f7f413e5179cf105
Brianconn71 <Brian.connolly71@gmail.com>
2021-04-22 11:37:23 +0000

    add: added a footer to the site and added a close button to the side nav

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // wanted to open the nav on the right of screen, answer found here https://stackoverflow.com/questions/37207668/how-do-i-open-a-materialize-sidenav-on-the-right-instead-of-the-left
     $('.sidenav').sidenav({
         edge:'right'
     });
