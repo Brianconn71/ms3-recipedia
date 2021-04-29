f3e834cbedfddebe034c98c379d1f4c4f2a6b052
Brianconn71 <Brian.connolly71@gmail.com>
2021-04-29 11:12:05 +0000

    add: added the necessary jquery to dynamically add and remove form fields

--- static/js/add_recipe.js
+++ static/js/add_recipe.js
@@ -1,6 +1,6 @@
 // got help and guidance on creating dynamic forms which I altered to suit my needs here: https://www.sanwebe.com/2013/03/addremove-input-fields-dynamically-with-jquery
 let ingred = 1;
-let max_ingred = 10
+let max_ingred = 20;
 
 $(".add_ingredient").click(function(e){
     e.preventDefault();
@@ -10,6 +10,37 @@ $(".add_ingredient").click(function(e){
         <div class="input-field col s12">
         <i class="fas fa-clipboard-list prefix shadow-text recipedia-text"></i>
         <input id="ingredients${ingred}" name="ingredients" minlength="2" maxlength="60" type="text" class="validate" required>
-        <label for="ingredients${ingred}">Ingredient ${ingred}:</label></div>`)
+        <label for="ingredients${ingred}">Ingredient ${ingred}:</label>
+        <a type="button" class="btn red remove_ingredient"><i class="far fa-minus-square"></i>Remove Ingredient</a></div>`)
     }
+})
+
+// got the same help and guidance which i changed to suit my needs as above: https://www.sanwebe.com/2013/03/addremove-input-fields-dynamically-with-jquery
+
+let step = 1;
+let max_steps = 10;
+
+$(".add_step").click(function(e){
+    e.preventDefault();
+    if(step < max_steps){
+        step ++;
+        $(".step_list").append(`
+        <div class="input-field col s12">
+        <i class="fas fa-list-ol prefix shadow-text recipedia-text"></i>
+        <input id="steps${step}" name="steps" minlength="3" maxlength="200" type="text" class="validate" required>
+        <label for="steps${step}">Step ${step}:</label>
+        <a type="button" class="btn red remove_step"><i class="far fa-minus-square"></i>Remove Step</a></div>`)
+    }
+})
+
+// below are two functions which remove the last added step/ingredient filed on the form
+// spent some time stuck on this but eventually got the guidance i needed here: https://www.bootstrapfriendly.com/blog/dynamically-add-or-remove-form-input-fields-using-jquery/
+$("body").on('click', ".remove_ingredient",function(){
+    $(this).parent('div').remove();
+    ingred--;
+})
+// remove step
+$("body").on('click', ".remove_step",function(){
+    $(this).parent('div').remove();
+    step--;
 })
