2189d563fb3e97169579289ba1852f82d35c90e0
Brianconn71 <Brian.connolly71@gmail.com>
2021-05-02 11:48:05 +0000

    Add: added a recipe categories page to display the various recipe categories

--- app.py
+++ app.py
@@ -178,6 +178,14 @@ def delete_recipe(recipe_id):
     flash("Recipe has been deleted successfully")
     return redirect(url_for("get_recipes"))
 
+
+@app.route("/get_categories")
+def get_categories():
+    categories = list(
+        mongo.db.categories.find().sort("category_name", 1))
+    return render_template("recipe_categories.html", categories=categories)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
