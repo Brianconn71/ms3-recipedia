400e9f6ef59a99c912ab0f4b3f5a9b6daa28082f
Brianconn71 <Brian.connolly71@gmail.com>
2021-05-12 15:12:25 +0000

    edit: edited styling to search bar and added links to login and register

---
+++ static/js/save_recipe.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    var $button = $('#save-recipe').clone();
+    $('#saved_recipe').html($button)
+    // $("#save_recipe").click(function(){
+    //     $(".card").clone().appendTo("#saved_recipe");
+    // });
+})
