89589976ff79e4ac8eadba88f62b7805d1bea72c
Brianconn71 <Brian.connolly71@gmail.com>
2021-05-12 11:39:58 +0000

    add: added some basic padding to the buttons on the homepage

--- static/js/script.js
+++ static/js/script.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
     });
     $('.modal').modal();
 
-    
+    // took the below form validation from the walkthrough project
     validateMaterializeSelect();
     function validateMaterializeSelect() {
         let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
