3b8adf67a3253b1a872ebab1749cc4f7d3d96652
Brianconn71 <Brian.connolly71@gmail.com>
2021-05-24 19:50:16 +0000

    edit: made some changes to the js code to adhere to jshint compliance

--- static/js/add_recipe.js
+++ static/js/add_recipe.js
@@ -1,3 +1,4 @@
+// jshint esversion: 6
 // got help and guidance on creating dynamic forms which I altered to suit my needs here: https://www.sanwebe.com/2013/03/addremove-input-fields-dynamically-with-jquery
 // The below functions add dynamic input fields to the forms
 let ingred = 1;
@@ -12,9 +13,9 @@ $(".add_ingredient").click(function(e){
         <i class="fas fa-clipboard-list prefix shadow-text recipedia-text"></i>
         <input id="ingredients${ingred}" name="ingredients" minlength="2" maxlength="60" type="text" class="validate" required>
         <label for="ingredients${ingred}">Ingredient ${ingred}:</label>
-        <a type="button" class="btn unsave-btn remove_ingredient"><i class="far fa-minus-square submit-icon"></i>Remove</a></div>`)
+        <a type="button" class="btn unsave-btn remove_ingredient"><i class="far fa-minus-square submit-icon"></i>Remove</a></div>`);
     }
-})
+});
 
 // got the same help and guidance which i changed to suit my needs as above: https://www.sanwebe.com/2013/03/addremove-input-fields-dynamically-with-jquery
 
@@ -30,18 +31,18 @@ $(".add_step").click(function(e){
         <i class="fas fa-list-ol prefix shadow-text recipedia-text"></i>
         <input id="steps${step}" name="steps" minlength="3" maxlength="500" type="text" class="validate" required>
         <label for="steps${step}">Step ${step}:</label>
-        <a type="button" class="btn unsave-btn remove_step"><i class="far fa-minus-square submit-icon"></i>Remove</a></div>`)
-    }
-})
+        <a type="button" class="btn unsave-btn remove_step"><i class="far fa-minus-square submit-icon"></i>Remove</a></div>`);
+   }
+});
 
 // below are two functions which remove the last added step/ingredient filed on the form
 // spent some time stuck on this but eventually got the guidance i needed here: https://www.bootstrapfriendly.com/blog/dynamically-add-or-remove-form-input-fields-using-jquery/
 $("body").on('click', ".remove_ingredient",function(){
     $(this).parent('div').remove();
     ingred--;
-})
+});
 // remove step
 $("body").on('click', ".remove_step",function(){
     $(this).parent('div').remove();
     step--;
-})
+});
--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+// jshint esversion: 6
 $(document).ready(function(){
     // wanted to open the nav on the right of screen, answer found here https://stackoverflow.com/questions/37207668/how-do-i-open-a-materialize-sidenav-on-the-right-instead-of-the-left
     $('.sidenav').sidenav({
